52b4998ae3e09b72d85e1fb96bc083795bf47591
gabriel.estefono <gabrielestefono@hotmail.com>
2026-04-12 13:31:30 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import ButtonComponent from "./components/ButtonComponent.vue";
+</script>
+
+<template>
+  <main>
+    <article class="container">
+      <header class="header">
+        <figure>
+          <img src="/public/profile-pic.png" alt="Jessica Randall" class="image" />
+          <figcaption class="sr-only">Thumbnail of social media profile</figcaption>
+        </figure>
+        <div>
+          <h1>Jessica Randall</h1>
+          <address>London, United Kingdom</address>
+        </div>
+      </header>
+      <section>
+        <p>"Front-end developer and avid reader."</p>
+      </section>
+      <footer>
+        <ButtonComponent>GitHub</ButtonComponent>
+        <ButtonComponent>FrontendMentor</ButtonComponent>
+        <ButtonComponent>LinkedIn</ButtonComponent>
+        <ButtonComponent>Twitter</ButtonComponent>
+        <ButtonComponent>Instagram</ButtonComponent>
+      </footer>
+    </article>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.container {
+  background-color: var(--gray-800);
+  width: 100%;
+  height: fit-content;
+  margin: var(--spacing-300);
+  padding: var(--spacing-300);
+  border-radius: var(--spacing-150);
+  display: flex;
+  flex-direction: column;
+  max-width: 24rem;
+}
+
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: var(--spacing-300);
+}
+
+.image {
+  width: 5.5rem;
+  height: 5.5rem;
+  border-radius: 50%;
+}
+
+.sr-only {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  white-space: nowrap;
+  border: 0;
+  clip-path: inset(50%);
+  clip: rect(0 0 0 0);
+}
+
+h1 {
+  font: var(--preset-01);
+  color: var(--white);
+}
+
+address {
+  font: var(--preset-02-bold);
+  color: var(--green);
+}
+
+section {
+  margin: var(--spacing-300) 0;
+  text-align: center;
+}
+
+footer {
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-200);
+}
+
+@media screen and (min-width: 600px) {
+  .container {
+    padding: var(--spacing-500);
+  }
+}
+</style>
---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+<template>
+  <a class="btn" href="#">
+    <slot></slot>
+  </a>
+</template>
+<style scoped>
+.btn {
+  width: 100%;
+  background-color: var(--gray-700);
+  color: var(--white);
+  font: var(--preset-02-bold);
+  text-decoration: none;
+  padding: var(--spacing-150);
+  border-radius: var(--spacing-100);
+  display: flex;
+  transition:
+    background-color 0.3s ease,
+    color 0.3s ease;
+  justify-content: center;
+}
+
+.btn:hover,
+.btn:focus,
+.btn:active,
+.btn:focus-visible {
+  background-color: var(--green);
+  color: var(--gray-700);
+
+  transition:
+    background-color 0.3s ease,
+    color 0.3s ease;
+}
+</style>
